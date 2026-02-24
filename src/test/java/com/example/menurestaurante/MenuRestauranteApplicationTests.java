892ca331b7db386076bbadfcf5c4fec6ceb0edc8
andrews <andrewsdosramos@gmail.com>
2026-02-24 13:07:25 +0100

    chore: inicializar proyecto

---
+++ src/test/java/com/example/menurestaurante/MenuRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.menurestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuRestauranteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
